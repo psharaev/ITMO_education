6bf20c77c6d94196fff973344751970d1a2ceca6
Pavel <mail@pechhenka.ru>
2022-06-18 12:48:37 +0300

    add java-advanced-tests

---
+++ java-advanced-tests/modules/info.kgeorgiy.java.advanced.implementor/info/kgeorgiy/java/advanced/implementor/JarImpler.java
@@ -0,0 +1,20 @@
+package info.kgeorgiy.java.advanced.implementor;
+
+import java.nio.file.Path;
+
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public interface JarImpler extends Impler {
+    /**
+     * Produces <var>.jar</var> file implementing class or interface specified by provided <var>token</var>.
+     * <p>
+     * Generated class' name should be the same as the class name of the type token with <var>Impl</var> suffix
+     * added.
+     *
+     * @param token type token to create implementation for.
+     * @param jarFile target <var>.jar</var> file.
+     * @throws ImplerException when implementation cannot be generated.
+     */
+    void implementJar(Class<?> token, Path jarFile) throws ImplerException;
+}
